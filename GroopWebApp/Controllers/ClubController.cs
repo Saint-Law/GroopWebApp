83b342f1a3e2af20f872dee713a1665ff6c742a3
Lawrence Peters <lawland2020@outlook.com>
2023-01-16 14:07:42 +0100

    Index and details page with logics and controller added

---
+++ GroopWebApp/Controllers/ClubController.cs
@@ -0,0 +1,33 @@
+﻿using GroopWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroopWebApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index() 
+        {
+            var clubs = _context.Clubs.ToList();
+            return View(clubs);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var club = _context.Clubs.Include( a => a.Address).FirstOrDefault(c => c.Id == id);
+            return View(club);
+        }
+    }
+}
---
+++ GroopWebApp/Controllers/RaceController.cs
@@ -0,0 +1,33 @@
+﻿using GroopWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroopWebApp.Controllers
+{
+    public class RaceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var races = _context.Races.ToList();
+            return View(races);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var race = _context.Races.Include(a => a.Address).FirstOrDefault(r => r.Id == id);
+            return View(race);
+        }
+    }
+}
